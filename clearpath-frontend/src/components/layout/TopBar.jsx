d8813761bdcc06b2ee3d8b1db41e725148431e52
Viessley <aekiveimar@gmail.com>
2026-03-19 21:26:57 -0400

    feat: replace emoji with SVG icons, add logo image to TopBar

--- clearpath-frontend/src/components/layout/TopBar.jsx
+++ clearpath-frontend/src/components/layout/TopBar.jsx
@@ -1,13 +1,11 @@
 import { HamburgerIcon, SettingsIcon, UserIcon } from "../../icons/System"
-
+import logo from '../../assets/clearpath-logo.png'
 
 export default function TopBar() {
   return (
     <div className="flex items-center justify-between px-5 py-4 bg-white border-b border-gray-100">
       <button className="text-gray-500"><HamburgerIcon size={22} /></button>
-      <h1 className="text-xl font-bold text-teal-700">
-        Clear<span className="text-teal-500">Path</span>
-      </h1>
+      <img src={logo} alt="ClearPath" style={{ height: 28 }} />
       <div className="flex gap-3 text-gray-500">
         <button><SettingsIcon size={22} /></button>
         <button><UserIcon size={22} /></button>
