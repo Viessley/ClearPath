329e3c92214f97c5f496d56d09905cd3b948c4c8
Viessley <aekiveimar@gmail.com>
2026-05-01 19:47:03 -0400

    refactor: remove scope-out complexity, clean up DecisionTreePage

--- clearpath-backend/src/main/java/com/clearpath/backend/repository/ScopeOutReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clearpath.backend.repository;
-
-import com.clearpath.backend.entity.ScopeOutReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScopeOutReportRepository extends JpaRepository<ScopeOutReport, String> {
-}
